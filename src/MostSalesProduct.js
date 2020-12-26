6ac2d653fad8a6af5d5e332b87091945155d7c54
arnastria <arnastria@gmail.com>
2020-12-26 21:28:36 +0700

    add most sales product componen

--- src/MostSalesProduct.js
+++ src/MostSalesProduct.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Fade from '@material-ui/core/Fade';
 import Title from './Title';
 
 function preventDefault(event) {
@@ -16,20 +17,69 @@ const useStyles = makeStyles({
 
 export default function MostSalesProduct() {
     const classes = useStyles();
+    const [data, setData] = useState({
+        productName:"Loading.."
+    });
+    const [show, setShow] = useState(true);
+    const [isPaused, setIsPaused] = useState(false);
+    const [counter, setCounter] = useState(0);
+    const ws = useRef(null);
+
+    useEffect(()=>{
+        ws.current = new WebSocket("wss://echo.websocket.org");
+        ws.current.onopen = () => console.log("ws openened");
+        ws.current.onclose = () => console.log("ws closed");        
+        return () => {
+            ws.current.close();
+        }
+    },[]);
+
+    useEffect(()=>{
+        if (!ws.current) return;
+        ws.current.onmessage = e => {
+            if (isPaused) return;
+            // const message = JSON.parse(e);
+            setData({
+                productName: e
+            })
+            console.log("etto",e);
+        }
+    },[isPaused])
+
+    useEffect(()=>{
+        const timer = setTimeout(() => {
+            setData({
+                productName: "nani "+counter
+            });
+            setShow(true);
+        }, 500);
+        return () => clearTimeout(timer);
+    },[show])
+
+    const sendMessage = ()=>{
+        setCounter(counter+1);
+        setShow(false);
+    };
+
     return (
         <React.Fragment>
             <Title>Most Sales Product</Title>
-            <Typography component="p" variant="h4">
-                Product name
-            </Typography>
+            <Fade in={show}>
+                <Typography component="p" variant="h4">
+                    {data.productName}
+                </Typography>
+            </Fade>
             <Typography color="textSecondary" className={classes.depositContext}>
-                on 15 March, 2019
+                on 32 December, 2020
             </Typography>
             <div>
                 <Link color="primary" href="#" onClick={preventDefault}>
                     View Detail
-        </Link>
+                </Link>
             </div>
+            <button onClick ={sendMessage}>
+                Nani
+            </button>
         </React.Fragment>
     );
 }
