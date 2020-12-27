22395b14bc601c6fa6c136cf71ab98e54f8e5eee
Farril Zavier Fernaldy <farrilf@gmail.com>
2020-12-27 20:28:15 +0700

    WIP: live chart updating

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Highcharts from "highcharts";
+import Highcharts from "highcharts/highstock";
 import HighchartsReact from "highcharts-react-official";
 import clsx from "clsx";
 import { withStyles } from "@material-ui/core/styles";
@@ -115,9 +115,15 @@ const themePDB = createMuiTheme({
     },
 });
 
-const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-const socket = new WebSocket("ws://" + window.location.hostname + ":8000" + "/visualization/");
+const chart1Socket = new WebSocket(
+    "ws://" + window.location.hostname + ":8000" + "/visualization/"
+);
+const chart2Socket = new WebSocket(
+    "ws://" + window.location.hostname + ":8000" + "/visualization/"
+);
+const recentRatingsSocket = new WebSocket(
+    "ws://" + window.location.hostname + ":8000" + "/visualization/"
+);
 
 class Dashboard extends Component {
     constructor(props) {
@@ -125,199 +131,44 @@ class Dashboard extends Component {
 
         this.state = {
             open: true,
-            newestRating: {},
-            newestAverageRating: {},
-            newestAverageAge: {},
-
-            chart1Options: {
-                title: {
-                    text: "Total Average Product Rating",
-                },
-                chart: {
-                    type: "line",
-                },
-                yAxis: {
-                    title: {
-                        text: "Average Rating",
-                    },
-                },
-                xAxis: {
-                    title: {
-                        text: "Periode",
-                    },
-                    categories: [],
-                },
-                series: [
-                    {
-                        name: "Average Rating",
-                        data: [],
-                    },
-                ],
-                responsive: {
-                    rules: [
-                        {
-                            condition: {
-                                maxWidth: 500,
-                            },
-                            chartOptions: {
-                                legend: {
-                                    layout: "horizontal",
-                                    align: "center",
-                                    verticalAlign: "bottom",
-                                },
-                            },
-                        },
-                    ],
-                },
-            },
-
-            chart2Options: {
-                title: {
-                    text: "Solar Employment Growth by Sector, 2010-2016",
-                },
-                yAxis: {
-                    title: {
-                        text: "Number of Employees",
-                    },
-                },
-                xAxis: {
-                    accessibility: {
-                        rangeDescription: "Range: 2010 to 2017",
-                    },
-                },
-                legend: {
-                    layout: "vertical",
-                    align: "right",
-                    verticalAlign: "middle",
-                },
-                plotOptions: {
-                    series: {
-                        label: {
-                            connectorAllowed: false,
-                        },
-                        pointStart: 2010,
-                    },
-                },
-                series: [
-                    {
-                        name: "Installation",
-                        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
-                    },
-                    {
-                        name: "Manufacturing",
-                        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
-                    },
-                    {
-                        name: "Sales & Distribution",
-                        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387],
-                    },
-                    {
-                        name: "Project Development",
-                        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227],
-                    },
-                    {
-                        name: "Other",
-                        data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111],
-                    },
-                ],
-                responsive: {
-                    rules: [
-                        {
-                            condition: {
-                                maxWidth: 500,
-                            },
-                            chartOptions: {
-                                legend: {
-                                    layout: "horizontal",
-                                    align: "center",
-                                    verticalAlign: "bottom",
-                                },
-                            },
-                        },
-                    ],
-                },
-            },
+            recentRatings: [],
         };
     }
 
-    componentDidMount() {
+    componentWillMount() {
         fetch("http://localhost:8000/");
 
-        socket.onopen = () => {
-            console.log("WebSocket Client Connected");
+        chart1Socket.onopen = () => {
+            console.log("Chart 1 WebSocket Client Connected");
         };
-        socket.onmessage = (message) => {
+        chart2Socket.onopen = () => {
+            console.log("Chart 2 WebSocket Client Connected");
+        };
+        recentRatingsSocket.onopen = () => {
+            console.log("Recent Ratings WebSocket Client Connected");
+        };
+
+        recentRatingsSocket.onmessage = (message) => {
             const data = JSON.parse(message.data);
-            console.log(data);
+            console.log("recent ratings socket message received");
 
             if (data.type === "rating") {
+                const { recentRatings } = this.state;
+                if (recentRatings.length >= 5) {
+                    recentRatings.shift();
+                }
+                recentRatings.push(data.message.after);
                 this.setState({
-                    newestRating: data.message,
-                });
-            } else if (data.type === "average_rating") {
-                this.setState(
-                    {
-                        newestAverageRating: data.message,
-                    },
-                    () => {
-                        const { categories } = this.state.chart1Options.xAxis;
-                        const { data } = this.state.chart1Options.series[0];
-
-                        if (categories !== undefined && data !== undefined) {
-                            if (categories.length >= 10 && data.length >= 10) {
-                                categories.shift();
-                                data.shift();
-                            }
-
-                            const timestamp = this.state.newestAverageRating.timestamp;
-                            const averageRating = this.state.newestAverageRating.average_rating;
-                            var datetime = new Date(timestamp);
-                            var month = months[datetime.getMonth()];
-                            var date = datetime.getDate();
-                            var hours = datetime.getHours();
-                            var mins = datetime.getMinutes();
-                            var secs = datetime.getSeconds();
-                            var millis = datetime.getMilliseconds();
-
-                            var dateString =
-                                month +
-                                " " +
-                                date +
-                                " " +
-                                hours +
-                                ":" +
-                                mins +
-                                ":" +
-                                secs +
-                                "." +
-                                millis;
-
-                            categories.push(dateString);
-                            data.push(averageRating);
-                            this.setState({
-                                chart1Options: {
-                                    xAxis: {
-                                        title: {
-                                            text: "Periode",
-                                        },
-                                        categories: categories,
-                                    },
-                                    series: [{ name: "Average Rating", data: data }],
-                                },
-                            });
-                        }
-                    }
-                );
-            } else if (data.type === "average_age") {
-                this.setState({
-                    newestAverageAge: data.message,
+                    recentRatings: recentRatings,
                 });
             }
         };
     }
 
     componentWillUnmount() {
-        socket.close();
+        chart1Socket.close();
+        chart2Socket.close();
+        recentRatingsSocket.close();
     }
 
     handleDrawerOpen = () => {
@@ -359,6 +210,153 @@ class Dashboard extends Component {
                 {"."}
             </Typography>
         );
+
+        const chart1Options = {
+            chart: {
+                events: {
+                    load: function () {
+                        const series = this.series[0];
+                        var x, y;
+                        chart1Socket.onmessage = (message) => {
+                            console.log("chart 1 socket message received");
+                            const data = JSON.parse(message.data);
+                            console.log(data);
+
+                            if (data.type === "average_rating") {
+                                x = new Date(data.message.timestamp).getTime();
+                                y = data.message.average_rating;
+                                const shift = series.data.length > 100;
+                                series.addPoint([x, y], true, shift);
+                            }
+                        };
+                    },
+                },
+            },
+            time: {
+                useUTC: false,
+            },
+            rangeSelector: {
+                buttons: [
+                    {
+                        count: 1,
+                        type: "second",
+                        text: "1S",
+                    },
+                    {
+                        count: 5,
+                        type: "second",
+                        text: "5S",
+                    },
+                    {
+                        count: 10,
+                        type: "second",
+                        text: "10S",
+                    },
+                    {
+                        count: 25,
+                        type: "second",
+                        text: "25S",
+                    },
+                    {
+                        count: 50,
+                        type: "second",
+                        text: "50S",
+                    },
+                    {
+                        type: "all",
+                        text: "All",
+                    },
+                ],
+                inputEnabled: false,
+                selected: 5,
+            },
+            title: {
+                text: "Total Average Product Rating",
+            },
+            yAxis: {
+                title: {
+                    text: "Average Rating",
+                },
+            },
+            xAxis: {
+                title: {
+                    text: "Timestamp",
+                },
+            },
+            series: [
+                {
+                    name: "Average Product Rating",
+                    data: [],
+                },
+            ],
+        };
+
+        const chart2Options = {
+            title: {
+                text: "Solar Employment Growth by Sector, 2010-2016",
+            },
+            yAxis: {
+                title: {
+                    text: "Number of Employees",
+                },
+            },
+            xAxis: {
+                accessibility: {
+                    rangeDescription: "Range: 2010 to 2017",
+                },
+            },
+            legend: {
+                layout: "vertical",
+                align: "right",
+                verticalAlign: "middle",
+            },
+            plotOptions: {
+                series: {
+                    label: {
+                        connectorAllowed: false,
+                    },
+                    pointStart: 2010,
+                },
+            },
+            series: [
+                {
+                    name: "Installation",
+                    data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
+                },
+                {
+                    name: "Manufacturing",
+                    data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
+                },
+                {
+                    name: "Sales & Distribution",
+                    data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387],
+                },
+                {
+                    name: "Project Development",
+                    data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227],
+                },
+                {
+                    name: "Other",
+                    data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111],
+                },
+            ],
+            responsive: {
+                rules: [
+                    {
+                        condition: {
+                            maxWidth: 500,
+                        },
+                        chartOptions: {
+                            legend: {
+                                layout: "horizontal",
+                                align: "center",
+                                verticalAlign: "bottom",
+                            },
+                        },
+                    },
+                ],
+            },
+        };
 
         return (
             <div className={classes.root}>
@@ -432,8 +430,9 @@ class Dashboard extends Component {
                                 <Grid item xs={12} md={4} lg={6}>
                                     <Paper className={classes.paper}>
                                         <HighchartsReact
+                                            constructorType={"stockChart"}
                                             highcharts={Highcharts}
-                                            options={this.state.chart1Options}
+                                            options={chart1Options}
                                         />
                                     </Paper>
                                 </Grid>
@@ -442,7 +441,7 @@ class Dashboard extends Component {
                                     <Paper className={classes.paper}>
                                         <HighchartsReact
                                             highcharts={Highcharts}
-                                            options={this.state.chart2Options}
+                                            options={chart2Options}
                                         />
                                     </Paper>
                                 </Grid>
